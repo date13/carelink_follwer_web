623dc069db1d892b4461459b308ce0746a5add79
bin liu <date13@hotmail.com>
2024-09-27 20:32:49 +0800

    修改提醒列表

--- web_page/src/views/components/notificationDialog.vue
+++ web_page/src/views/components/notificationDialog.vue
@@ -2,12 +2,24 @@
   <el-dialog :close-on-press-escape="false" :destroy-on-close="true" :model-value="show"
              :show-close="false" title="警告列表" width="60%" @close="close">
     <div class="flex flex-col h-100">
-      <div class="w-full h-full flex-1 notification-panel">
-        <el-alert v-for="{messageId,sg,triggeredDateTime} in list.sort((a:any,b:any)=>{
+      <div v-if="notificationHistory.activeNotifications.length>0" class="mx-3 mb-2">
+        <div class="mb-1">当前警报:</div>
+        <el-alert v-for="{messageId,sg,triggeredDateTime} in notificationHistory.activeNotifications.sort((a:any,b:any)=>{
           return sugarCalc.cleanTime(b.triggeredDateTime) - sugarCalc.cleanTime(a.triggeredDateTime)
         })" :closable="false"
                   :description="dayjs(sugarCalc.cleanTime(triggeredDateTime)).format(DATE_FORMAT.datetime)"
-                  :title="NOTIFICATION_MAP[messageId]?showMsg(messageId,sg):messageId"
+                  :title="NOTIFICATION_MAP[messageId]?sugarCalc.showNotificationMsg(messageId,sg):messageId"
+                  :type="NOTIFICATION_MAP[messageId]?NOTIFICATION_MAP[messageId].type:'warning'"
+                  class="item" show-icon/>
+      </div>
+
+      <div class="ml-3 mb-1">历史警报:</div>
+      <div class="w-full h-full flex-1 notification-panel">
+        <el-alert v-for="{messageId,sg,triggeredDateTime} in notificationHistory.clearedNotifications.sort((a:any,b:any)=>{
+          return sugarCalc.cleanTime(b.triggeredDateTime) - sugarCalc.cleanTime(a.triggeredDateTime)
+        })" :closable="false"
+                  :description="dayjs(sugarCalc.cleanTime(triggeredDateTime)).format(DATE_FORMAT.datetime)"
+                  :title="NOTIFICATION_MAP[messageId]?sugarCalc.showNotificationMsg(messageId,sg):messageId"
                   :type="NOTIFICATION_MAP[messageId]?NOTIFICATION_MAP[messageId].type:'warning'"
                   class="item" show-icon/>
       </div>
@@ -32,8 +44,12 @@ const props = defineProps({
     default: false,
     type: Boolean
   },
-  list: {
-    default: []
+  notificationHistory: {
+    default: {
+      clearedNotifications: [],
+      activeNotifications: []
+    },
+    type: Object
   }
 })
 
@@ -41,12 +57,6 @@ const emit = defineEmits(["update:show"])
 const sugarCalc = useSugarCalc()
 onMounted(() => {
 })
-
-function showMsg(messageId, sg) {
-  const item = NOTIFICATION_MAP[messageId]
-  if (sg) return item.text.replace(item.replace, sugarCalc.calcSG(sg))
-  return item.text
-}
 
 function close() {
   emit("update:show", false);
@@ -61,6 +71,18 @@ function close() {
 
   .item {
     margin: 0 10px 10px 0;
+
+    :deep(.el-alert__icon) {
+      font-size: 18px;
+    }
+
+    :deep(.el-alert__title) {
+      font-size: 13px !important;
+    }
+
+    :deep(.el-alert__description) {
+      font-size: 12px !important;
+    }
   }
 }
 </style>
--- web_page/src/views/test/test.vue
+++ web_page/src/views/test/test.vue
@@ -1,11 +1,14 @@
 <template>
   <MainPanel>
     <el-button type="primary" @click="testApi">test api limit</el-button>
+    <el-button type="primary" @click="test">test</el-button>
   </MainPanel>
 </template>
 <script lang="ts" name="Text" setup>
 import MainPanel from "@/layout/components/MainPanel.vue";
 import {TestService} from "@/service/test-service";
+import {Msg} from "@/utils/tools";
+import CryptoJS from "crypto-js";
 
 const service = new TestService()
 
@@ -14,6 +17,10 @@ function testApi() {
   console.log(result);
 }
 
+function test() {
+  Msg.successMsg("ok")
+  console.log(CryptoJS.HmacSHA1(JSON.stringify({a: 1}), 'key').toString());
+}
 </script>
 
 <style lang="scss" scoped>
