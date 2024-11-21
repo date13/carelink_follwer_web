63e8d93f2b12f9b12ee936e80439712d88801c42
liubin <liubin@baoflag.com>
2024-11-21 15:57:37 +0800

    添加sse接口

--- web_service/router/sugar.py
+++ web_service/router/sugar.py
@@ -1,30 +1,54 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import asyncio
+import json
+from datetime import datetime
+
 from fastapi import APIRouter
 from starlette.requests import Request
-
+from sse_starlette import EventSourceResponse, ServerSentEvent
 from core.route_log import LogRoute
 from models.result import Result, ResultSchema, RedisHashObj
 from service.sugarService import loadCarelinkData, getCarelinkToken, refreshCarelinkDataForUser, \
     refreshCarelinkTokenForUser, \
     getAllFood, updateFood, delFood
-from service.sysService import getSysDict, getSysDictForUser
+from service.sysService import getSysDictForUser
 
 router = APIRouter(route_class=LogRoute)
 
 
-@router.put("/")
-def sugar(request: Request) -> ResultSchema:
-    user = request.state.user
-    data = getSysDictForUser(user, 'carelinkData')
-    myData = getSysDictForUser(user, 'carelinkMyData')
+def loadSugarData(user, isSSE=False):
     try:
+        data = getSysDictForUser(user, 'carelinkData')
+        myData = getSysDictForUser(user, 'carelinkMyData')
         return Result.success(data={
             "data": data,
             "myData": myData
-        })
+        }) if not isSSE else {"data": json.loads(data), "myData": json.loads(myData)}
     except Exception as ex:
         return Result.fail(msg='用户:%s 获取 carelink 数据错误:%s' % (user, str(ex)))
+
+
+async def sugarSSEData(user):
+    while True:
+        result = loadSugarData(user, True)
+        yield ServerSentEvent(
+            event=f"{user}:sugar_data",
+            id=str(datetime.now().timestamp()),
+            data=json.dumps(result)
+        )
+        await asyncio.sleep(50)
+
+@router.put("/")
+def sugar(request: Request) -> ResultSchema:
+    user = request.state.user
+    return loadSugarData(user)
+
+
+@router.put("/sse")
+def sugarSSE(request: Request):
+    user = request.state.user
+    return EventSourceResponse(content=sugarSSEData(user))
 
 
 @router.put("/loadCarelinkData")
