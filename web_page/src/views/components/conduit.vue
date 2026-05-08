22bc36bf7720a5b466b366c4cbb24ad2026b8199
liubin <liubin@baoflag.com>
2026-05-08 14:13:59 +0800

    显示管路上一次的启动时间

--- web_page/src/views/components/conduit.vue
+++ web_page/src/views/components/conduit.vue
@@ -1,69 +1,81 @@
 <template>
-  <el-popover
-      :content="`管路启用:${dayjs(lastUpdateTime.conduitDatetime).format(DATE_FORMAT.datetime2)}`"
-      placement="bottom"
-      trigger="hover"
-  >
-    <template #reference>
-      <div class="flex">
-        <el-tag class="hand mr-2" size="small" type="warning" @click="updateConduitDatetime()">管路:
-          {{ lastUpdateTime.conduit }}
-        </el-tag>
+    <el-popover
+        :content="`管路启用:${dayjs(lastUpdateTime.conduitDatetime).format(DATE_FORMAT.datetime2)}`"
+        placement="bottom"
+        trigger="hover"
+    >
+        <template #reference>
+            <div class="flex">
+                <el-tag
+                    class="hand mr-2"
+                    size="small"
+                    type="warning"
+                    @click="updateConduitDatetime"
+                    >管路:
+                    {{ lastUpdateTime.conduit }}
+                </el-tag>
 
-        <ep-Setting class="hand" @click="openConduitDate"></ep-Setting>
-        <el-date-picker
-            ref="datetimeRef"
-            v-model="dateTime"
-            :default-time="new Date()"
-            :show-now="false"
-            class="conduit-datetime "
-            placeholder="选择日期时间"
-            type="datetime"
-            @change="submitConduitTime"
-        >
-        </el-date-picker>
-      </div>
-    </template>
-  </el-popover>
+                <ep-Setting class="hand" @click="openConduitDate"></ep-Setting>
+                <el-date-picker
+                    ref="datetimeRef"
+                    v-model="dateTime"
+                    :default-time="new Date()"
+                    :show-now="false"
+                    class="conduit-datetime"
+                    placeholder="选择日期时间"
+                    type="datetime"
+                    @change="submitConduitTime"
+                >
+                </el-date-picker>
+            </div>
+        </template>
+    </el-popover>
 </template>
 <script lang="ts" name="Conduit" setup>
 import dayjs from "dayjs";
-import {DATE_FORMAT} from "@/model/model-type";
-import {ElDatePicker} from "element-plus";
+import { DATE_FORMAT } from "@/model/model-type";
+import { ElDatePicker } from "element-plus";
 
-const emit = defineEmits(['updateConduitDatetime'])
+const emit = defineEmits(["updateConduitDatetime"]);
 const props = defineProps({
-  lastUpdateTime: {
-    default: {},
-  },
-  type: Object
-})
-const dateTime = ref('')
-const datetimeRef = ref(ElDatePicker)
+    lastUpdateTime: {
+        default: {},
+    },
+    type: Object,
+});
+const dateTime = ref("");
+const datetimeRef = ref(ElDatePicker);
+const getParams = () => {
+    return {
+        startTime: dayjs(props.lastUpdateTime.conduitDatetime).format(
+            DATE_FORMAT.datetime2,
+        ),
+    };
+};
 
-function updateConduitDatetime(params: any) {
-  emit('updateConduitDatetime', params)
+function updateConduitDatetime() {
+    emit("updateConduitDatetime", getParams());
 }
 
 function openConduitDate() {
-  datetimeRef.value.focus()
+    datetimeRef.value.focus();
 }
 
 function submitConduitTime() {
-  console.log(dateTime);
-  emit('updateConduitDatetime', {
-    datetime: dateTime.value
-  })
-  datetimeRef.value.blur()
+    emit("updateConduitDatetime", {
+        datetime: dateTime.value,
+        ...getParams(),
+    });
+    datetimeRef.value.blur();
 }
 </script>
 
 <style lang="scss">
 .conduit-datetime {
-  width: 0;
-  height: 0;
-  position: fixed;
-  bottom: 0px;
-  z-index: -1;
+    width: 0;
+    height: 0;
+    position: fixed;
+    bottom: 0px;
+    z-index: -1;
 }
 </style>
